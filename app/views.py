d18d1a2648dc302e1a542efd1edcc01fba469267
supriyapotli987 <supriyapotli987@gmail.com>
2023-07-28 12:19:34 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def data_render(request):
-    d={'name':'supriya','age':3}
+    d={'name':'supriya','age':23}
     return render(request,'data_render.html',context=d)
+
+
+def ifconditions(request):
+    d={'a':190,'b':123,'c':9000}
+    return render(request,'ifconditions.html',context=d)
--- project11/urls.py
+++ project11/urls.py
@@ -20,4 +20,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('data_render/',data_render,name='data_render'),
+    path('ifconditions/',ifconditions,name='ifconditions'),
 ]
